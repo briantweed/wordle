fa876b30d35f08dc336a6835c0a792ecb9021903
Brian Tweed <brian.tweed@simplyioa.com>
2024-08-12 11:35:34 +0100

    gogogo

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import {useEffect, useMemo, useState} from "react";
 import {WORD_LIST, POSSIBLE_WORDS} from "@config/wordle";
+import Head from "next/head";
 
 
 const Wordle = () => {
@@ -178,6 +179,22 @@ const Wordle = () => {
     return (
         <div id="wordle">
 
+            <Head>
+                <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
+                <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
+                <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
+                <link rel="manifest" href="/site.webmanifest"/>
+                <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5"/>
+                <meta name="msapplication-TileColor" content="#2b5797"/>
+                <meta name="theme-color" content="#ffffff"/>
+                <link rel="preconnect" href="https://fonts.google.com"/>
+                <link rel="preconnect" href="https://fonts.googleapis.com"/>
+                <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin/>
+                <link
+                    href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@100..900&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&family=Salsa&display=swap"
+                    rel="stylesheet"/>
+            </Head>
+
             <div className="nav">
                 <div className="streak" title={wordle}>Streak: {streak}</div>
                 {gameOver && <div className={"word"}>{wordle}</div>}
@@ -192,7 +209,7 @@ const Wordle = () => {
                             {[...Array(MAX_WORD_LENGTH)].map((letter, pos) => {
                                 let className = stats[index] !== undefined && [pos] !== undefined ? stats[index][pos] : "";
                                 return (
-                                    <div className={`letter ${className}`}
+                                    <div className={`letter roboto-medium ${className}`}
                                          key={pos}>{word[pos] ? word[pos].toUpperCase() : ""}</div>
                                 );
                             })}
