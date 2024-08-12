3f8161f8d08b0e2fbed3819623e24d70576e5309
Brian Tweed <brian.tweed@simplyioa.com>
2024-08-12 18:42:36 +0100

    gogogo

--- pages/index.js
+++ pages/index.js
@@ -17,7 +17,7 @@ const Wordle = () => {
     const KEYBOARD_LAYOUT = useMemo(() => [
         ["q", "w", "e", "r", "t", "y", "u", "i", "o", "p"],
         ["a", "s", "d", "f", "g", "h", "j", "k", "l"],
-        ["<", "z", "x", "c", "v", "b", "n", "m", "enter"]
+        ["enter", "z", "x", "c", "v", "b", "n", "m", "<"]
     ], []);
 
     const CLASS_NAMES = {
