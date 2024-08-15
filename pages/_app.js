e7b37c70270a1bff05be943fbb9435514edf41be
Brian Tweed <brian.tweed@simplyioa.com>
2024-08-15 11:53:44 +0100

    gogogo

--- pages/_app.js
+++ pages/_app.js
@@ -34,10 +34,6 @@ export default function App({Component, pageProps}) {
                 <link rel="manifest" href="/site.webmanifest"/>
                 <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#367787"/>
 
-                <link rel="preconnect" href="https://fonts.google.com"/>
-                <link rel="preconnect" href="https://fonts.googleapis.com"/>
-                <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin/>
-                <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@100..900&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900display=swap"/>
             </Head>
 
             <Component {...pageProps} />
--- pages/_document.js
+++ pages/_document.js
@@ -3,11 +3,17 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
+        <Head>
+            <link rel="preconnect" href="https://fonts.google.com"/>
+            <link rel="preconnect" href="https://fonts.googleapis.com"/>
+            <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true"/>
+            <link rel="stylesheet"
+                  href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@100..900&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900display=swap"/>
+        </Head>
+        <body>
+        <Main/>
+        <NextScript/>
+        </body>
     </Html>
   );
 }
