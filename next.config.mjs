fc11aea8d1118d3db430fba5b6f2256c532930ba
Brian Tweed <brian.tweed@simplyioa.com>
2024-08-15 19:44:50 +0100

    gogogo

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,18 @@
+import * as pwa from "@ducanh2912/next-pwa";
+
+const withPWA = pwa.default({
+    dest: 'public',
+    cacheOnFrontEndNav: true,
+    aggresiveFrontEndNavCaching: true,
+    reloadOnOnline: true,
+    swcMinify: true,
+    fallback: {
+        document: "/offline"
+    }
+});
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+    reactStrictMode: true,
 };
 
-export default nextConfig;
+export default withPWA(nextConfig);
